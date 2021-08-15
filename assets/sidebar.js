70049855d0e23406c1578925a57c7cec1d75e036
Steven Quintana <steve.e.quint@gmail.com>
2021-08-14 18:52:11 -0700

    Comment out unused js file

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,8 +1,8 @@
-/* global bootstrap: false */
-(function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
+// /* global bootstrap: false */
+// (function () {
+//   'use strict'
+//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//   tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+//     new bootstrap.Tooltip(tooltipTriggerEl)
+//   })
+// })()
