0d6de8edb5ec652da66b7e4908828c4a6cd45c90
Steven Quintana <steve.e.quint@gmail.com>
2021-08-15 09:21:17 -0700

    Build character objects

---
+++ assets/app.js
@@ -0,0 +1,88 @@
+let round1 = true
+let round2 = false
+let round3 = false
+let round4 = false
+
+let charSteven = {
+  powers: [
+    {name: 'charm', moodPoints: 1},
+    {name: 'programming', moodPoints: 3},
+    {name: 'surfing', moodPoints: 4},
+    {name: 'taking vacations', moodPoints: 6}
+  ]
+  stamina: 100,
+  money: 50,
+  level: 1,
+  unlockedChars: [],
+  inCircle: false
+}
+
+let charMia = {
+  powers: [
+    {name: 'math', moodPoints: 2},
+    {name: 'guitar', moodPoints: 5},
+    {name: 'baking', moodPoints: 6},
+    {name: 'good-jokes', moodPoints: 7}
+  ],
+  weaknesses: ['vacations', 'baked goods', 'hanging out with friends', 'charm', 'money'],
+  dislikes: ['bad-jokes', 'misogynists', 'viking-fans']
+  mood: ['happy', 'neutral', 'angry'],
+  moodPoints,
+  money: 0
+}
+
+let charHannah = {
+  powers: [
+    { name: 'adapting', moodPoints: 2 },
+    { name: 'sarcasm', moodPoints: 4 },
+    { name: 'cooking', moodPoints: 5 },
+    { name: 'nursing', moodPoints: 8 }
+  ],
+  weaknesses: ['zach', 'house-gifts', 'greys-anatomy', 'good-jokes'],
+  dislikes: ['bad-jokes', 'misogynists', 'drunkeness']
+  mood: ['happy', 'neutral', 'angry'],
+  moodPoints,
+  money: 50
+}
+
+let charZach = {
+  powers: [
+    { name: 'golfing', moodPoints: 3 },
+    { name: 'go-with-the-flow', moodPoints: 4 },
+    { name: 'drinking-ipas', moodPoints: 6 },
+    { name: 'viking-knowledge', moodPoints: 10 }
+  ],
+  weaknesses: ['vikings', 'hannah', 'ipa', 'greys-anatomy'],
+  dislikes: ['chaos', 'packer-fans']
+  mood: ['happy', 'neutral', 'angry'],
+  moodPoints,
+  money: 50
+}
+
+let charPapaBear = {
+  powers: [
+    { name: 'bad-jokes', moodPoints: 1 },
+    { name: 'bike-riding', moodPoints: 4 },
+    { name: 'drinking', moodPoints: 8 },
+    { name: 'cooking', moodPoints: 10 }
+  ],
+  weaknesses: ['sleep', 'whiskey', 'the-girls'],
+  dislikes: ['american-sports', 'messy-house', 'ipas', 'steven', 'money', 'good-jokes', 'bad-service']
+  mood: ['happy', 'neutral', 'angry'],
+  moodPoints,
+  money: 200
+}
+
+let charMamaBear = {
+  powers: [
+    { name: 'finance', moodPoints: 5 },
+    { name: 'herbal-knowledge', moodPoints: 6 },
+    { name: 'organization', moodPoints: 8 },
+    { name: 'mama-bear-ferocity', moodPoints: 10 }
+  ],
+  weaknesses: ['left-alone'],
+  dislikes: ['american-sports', 'messy-house', 'bad-jokes', 'steven', 'money', 'bad-service', 'loud-music', 'long-hair']
+  mood: ['happy', 'neutral', 'angry'],
+  moodPoints,
+  money: 300
+}
