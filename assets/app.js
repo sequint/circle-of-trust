0ac0b16ebcd19bba62bb083b9347c64f80ee68eb
Steven Quintana <steve.e.quint@gmail.com>
2021-08-15 10:49:19 -0700

    Create a message loop that loops through an array of messages on each next click

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,23 @@ let round4 = false
 $(window).load(function () {
   $('#instruction-start').modal('show');
 });
+
+// Create variable to count which click you are on.
+let nextClick = 0;
+
+// Create array of the instruction messages.
+const instructions = [
+  'The circle of trust is complex, it has 3 outer rings surrounding the inner circle.  Each ring is guarded, as well as the inner circle.',
+  'Steven must pass the test of each guardian in order to progress into the next ring.',
+  "Use Steven's powers to to try winning over each guardian.  If the guardian is won over, they will allow you safe passage and will become your alley.  When a guardian becomes your alley you can access their powers up to 3 times.",
+  "But be warned, Steven only has a limited amount of stamina until he disovles into perfect entropy.  Each guardians verbal attacks can damage Steven's stamina.",
+  'Are you ready to play?'
+]
+
+document.getElementById('decree-next-btn').addEventListener('click', event => {
+  event.preventDefault()
+
+  document.getElementById('start-instructions-content').textContent = instructions[nextClick]
+  nextClick++
+
+})
