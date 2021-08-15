617338c8d174a8b9aec677fcf03a21d8eba60a9f
Steven Quintana <steve.e.quint@gmail.com>
2021-08-15 11:25:17 -0700

    Add middle finger emoji on I Hate Steven click

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@ $(window).load(function () {
 });
 
 // Create variable to count which click you are on.
-let nextClick = 0;
+let nextClick = 0
 
 // Create array of the instruction messages.
 const instructions = [
@@ -24,6 +24,27 @@ document.getElementById('decree-next-btn').addEventListener('click', event => {
   event.preventDefault()
 
   document.getElementById('start-instructions-content').textContent = instructions[nextClick]
-  nextClick++
 
+  if (nextClick === 4) {
+    document.getElementById('close-btn-cont').innerHTML = `
+    <button type="button" class="btn btn-secondary hate-steven-btn" id="hate-steven-btn" data-bs-dismiss="modal">I Hate Steven</button>
+    `
+    document.getElementById('decree-next-btn').textContent = "Let's Do This!"
+  }
+  else {
+    nextClick++
+  }
+
+})
+
+// Event listener for hate steven button to throw up a middle finger and close on click.
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('hate-steven-btn')){
+    console.log('Middle finger')
+
+    document.getElementById('middle-finger-pic').innerHTML = `
+    <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/apple/96/reversed-hand-with-middle-finger-extended_1f595.png" alt="Middle finger emoji">
+    `
+    
+  }
 })
